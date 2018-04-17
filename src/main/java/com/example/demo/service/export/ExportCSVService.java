ffa3672cfd6718a4c46ec660f78a31d2a8c405f4
Leovi <leovi.nutakor@cgi.com>
2018-04-17 14:55:25 +0200

    CSV -> reflection

--- src/main/java/com/example/demo/service/export/ExportCSVService.java
+++ src/main/java/com/example/demo/service/export/ExportCSVService.java
@@ -5,20 +5,49 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.io.Writer;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map.Entry;
 
 @Service
 public class ExportCSVService {
+	
+	public final String SEPARATOR = ";"; 
 
     public void export(Writer printWriter, List<ClientDTO> clients) throws IOException {
-        printWriter.write("Nom;");
-        printWriter.write("Prenom;");
-        for (ClientDTO client : clients) {
-            printWriter.write(client.getNom());
-            printWriter.write(";");
-            printWriter.write(client.getPrenom());
-            printWriter.write("\n");
-        }
-
+    	
+    	Class<? extends ClientDTO> classType = clients.get(0).getClass();
+    	Method[] classMethods = classType.getDeclaredMethods();
+    	
+    	HashMap<String, Method> getters = new HashMap<>(); 
+    	for(Method m : classMethods) {
+    		String name = m.getName();
+    		if(name.startsWith("get")) {
+    			getters.put(name.substring(3), m);
+    		}
+    	}
+    	
+    	for(Entry<String, Method> set : getters.entrySet()) {
+    		printWriter.write(set.getKey() + SEPARATOR);// write headers
+    	}
+    	
+    	for (ClientDTO client : clients) {
+    		
+    		printWriter.write("\n");
+    		
+    		for(Entry<String, Method> set : getters.entrySet()) {
+    			try {
+					String gottenVar = set.getValue().invoke(client, new Object[]{}).toString();
+					printWriter.write(gottenVar + ";");
+				} catch (Exception e) {
+					// blub
+				}
+        	}
+         }
     }
 }
